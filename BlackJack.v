3a12d13257b9412c6a060cd620e09a50d9f3f36f
Kelvin Duong <duongke8@IITS-I402-19.utsc-labs.utoronto.ca>
2018-03-20 17:54:40 -0400

    Modified BlackJck.v

--- BlackJack.v
+++ BlackJack.v
@@ -5,23 +5,23 @@ module BlackJack(SW, HEX0, HEX1, HEX2, HEX3, CLOCK_50, KEY, LEDR);
 	output [6:0] HEX0, HEX1, HEX2, HEX3;
 	output [9:0] LEDR;
 	
-	//wire [3:0] rand_num;
-	hex7seg h0(.IN(SW[7:0]),  //Add 4'b0000 b/c rand_num is 4 bit
+	wire [3:0] rand_num;
+	hex7seg h0(.IN({3'b000, rand_num}),  //Add 4'b0000 b/c rand_num is 4 bit
 		       .OUT0(HEX1[6:0]), 
 		       .OUT1(HEX0[6:0]));
 		       
 	hex7seg h1(.IN(SW[14:8]), .OUT0(HEX3[6:0]), .OUT1(HEX2[6:0]));
 	
-	//counter c0(.enable(1'b1),  //Counter always counting from 1-10
-	//	  .clock(CLOCK_50),
-		//  .reset_n(KEY[3]),
-		//  .q(rand_num),
-		//  .load(KEY[1])
-	//	);
+	counter c0(.enable(1'b1),  //Counter always counting from 1-10
+		  .clock(CLOCK_50),
+		  .reset_n(KEY[3]),
+		  .q(rand_num),
+		  .load(KEY[1])
+		);
 		
-	//statemachine bj(
-	//	.Clock(CLOCK_50), .Reset(KEY[3]), .enter(KEY[2]), .pass(KEY[1]), .fsm_output(LEDR[0])
-	//);
+	statemachine bj(
+		.Clock(CLOCK_50), .Reset(KEY[3]), .enter(KEY[2]), .pass(KEY[1]), .fsm_output(LEDR[0])
+	);
 
 endmodule
 
