0bcf9f338282c369747c5bd2b841c083128fe947
kelvin3094 <37411858+kelvin3094@users.noreply.github.com>
2018-03-22 22:01:00 -0400

    Add files via upload
    Fixed some states.

--- statemachine.v
+++ statemachine.v
@@ -6,7 +6,7 @@ module statemachine(Clock, reset_n, enter, pass, phand, dhand, fsm_out);  //Deal
     input Clock, reset_n, enter, pass;
     output reg [1:0] fsm_out == 2'b00; //initialized the starting value of the output
     
-    wire [4:0] prand_num, drand_num;
+    wire [4:0] prand_num, drand_num; //maybe change wire to a register
     
     counter c0(.enable(1'b1), 
 		  .clock(Clock), //used CLOCK instead of clock. that's why the random number didn't change
@@ -38,7 +38,7 @@ module statemachine(Clock, reset_n, enter, pass, phand, dhand, fsm_out);  //Deal
                     else
                         state <= 3'b000;
             3'b001: begin
-                        if(enter == 1'b0)
+                        if(enter == 1'b0)  //player draws card until they pass or bust
                             begin
 				phand = phand + prand_num;
                                 // add card to players hand
@@ -47,20 +47,16 @@ module statemachine(Clock, reset_n, enter, pass, phand, dhand, fsm_out);  //Deal
                                 else
                                     state <= 3'b011;
                             end
-                        else if(pass == 1'b0)
+                        else if(pass == 1'b0)  //dealer draws a card everytime pass is pressed, until they bust
                             begin
-				if(dhand < phand)
-				begin
-				dhand = dhand + drand_num;
-				tate <= 3'b001;
-				end
+			        dhand = dhand + drand_num;
                                 if(dhand > 5'b10101)
-                                    state <= 3'b101;
+                                    state <= 3'b101;   //player wins  
                                 else if(phand > dhand)
-                                    state <= 3'b101;
+                                    state <= 3'b011;   //player loses
 				else
-				    state <= 3'b011; //LOSE STATE INSTEAD
-				end
+				    state <= 3'b101; //dealer and player have the same score so just let the player win
+				end                  //it's different in real blackjack though
                     end
             3'b011:  // player lose
                         if(reset_n == 1'b0)
