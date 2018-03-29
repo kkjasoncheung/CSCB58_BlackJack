a332d65b70af5c7a37860874c5d946348c6a795c
kelvin3094 <37411858+kelvin3094@users.noreply.github.com>
2018-03-29 16:58:32 -0400

    Add files via upload
    Without the clock in the always block.

--- statemachine.v
+++ statemachine.v
@@ -17,7 +17,7 @@ module statemachine(Clock, reset_n, enter, pass, phand, dhand, fsm_out, prandnum
 		);
 
 
-    always@(negedge enter or negedge pass or negedge reset_n or posedge Clock) //try always@(*) or always@(posedge Clock)
+    always@(negedge enter or negedge pass or negedge reset_n) //try always@(*) or always@(posedge Clock)
     begin: FSM
         case (state)
             3'b000:begin
